af41a1853b56b203be06eaeedaee78a57c947f88
JiangHu <2748599498@qq.com>
2025-06-29 05:08:45 +0000

    update src/router/index.js.
    Signed-off-by: JiangHu <2748599498@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,22 @@ const router=createRouter({
         { path:'/tiezishenhe',  name:'tiezishenhe',  component:()=>import('../views/Tiezishenhe.vue')  },
         { path:'/zhiyuanbaoming',  name:'zhiyuanbaoming',  component:()=>import('../views/Zhiyuanbaoming.vue')  },
         { path:'/login',  name:'login',  component:()=>import('../views/LogIn.vue')  },
-        {path:'/Gongyizixun',name:'Gongyizixun',component:()=>import('../views/Gongyizixun.vue')}
+        {path:'/Gongyizixun',name:'Gongyizixun',component:()=>import('../views/Gongyizixun.vue')},
+        {
+        path: '/news/2',
+        name: 'NewsDetail2',
+        component: () => import('@/views/NewsDetail2.vue')
+    },
+    {
+        path: '/news/3',
+        name: 'NewsDetail3',
+        component: () => import('@/views/NewsDetail3.vue')
+    },
+    {
+        path: '/news/4',
+        name: 'NewsDetail4',
+        component: () => import('@/views/NewsDetail4.vue')
+    }
     ]
 })
 
