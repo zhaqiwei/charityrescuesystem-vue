c088debb42677b91f6bf952243e9079682c41b47
xihuanhua <xihuanhua@hua.com>
2025-07-01 12:34:23 +0800

    更新依赖

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import path from 'path';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    // 添加 Node.js 标准库的浏览器 polyfill
     nodePolyfills({
-      // 只包含需要的模块
+      // 仅 polyfill 需要的 Node.js 模块
       include: ['path'],
+      // 启用 Node.js 全局变量
+      globals: {
+        Buffer: true, // 启用 Buffer 全局变量
+        global: true, // 启用 global 全局变量
+        process: true, // 启用 process 全局变量
+      },
     }),
   ],
   resolve: {
     alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url))
-      '@': path.resolve(__dirname,'src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+      // '@': path.resolve(__dirname,'src')
     },
   },
   // 优化依赖
   optimizeDeps: {
     include: ['element-plus'],
+    esbuildOptions: {
+      plugins: [
+      ],
+    },
   },
   // 构建配置
   build: {
@@ -32,4 +41,4 @@ export default defineConfig({
       external: ['node:url'], // 排除 Node.js 内置模块
     },
   },
-})
+});
